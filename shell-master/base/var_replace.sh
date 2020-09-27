fc4fd5cdc2e94ae87f016649485efa0a6d98d50b
ydong08 <lfeng1108@163.com>
2020-09-27 17:34:36 +0800

    update code

--- shell-master/base/var_replace.sh
+++ shell-master/base/var_replace.sh
@@ -15,6 +15,7 @@ echo "\${var:+msg}: ${name:+'word'} <=> echo $name"
 echo "\${var:-msg}: ${name:-'word'} <=> echo $name"
 echo "\${var:=msg}: ${name:='word'} <=> echo $name"
 echo "\${var:?msg}: ${name:?'word'} <=> echo $name"
+echo "-------------------------------------------\n"
 
 # 无值
 echo "\${var+msg}: ${value+'word'} <=> echo $value"
@@ -26,6 +27,7 @@ echo "\${var:+msg}: ${value:+'word'} <=> echo $value"
 echo "\${var:-msg}: ${value:-'word'} <=> echo $value"
 echo "\${var:=msg}: ${value:='word'} <=> echo $value"
 echo "\${var:?msg}: ${value:?'word'} <=> echo $value"
+echo "-------------------------------------------\n"
 
 # 无定义
 echo "\${var+msg}: ${rep+'word'} <=> echo $rep"
