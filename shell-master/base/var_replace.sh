a36b7e06217c17cd2154c59eb95ac032a6f4cb1b
ydong08 <lfeng1108@163.com>
2020-09-27 17:32:57 +0800

    update code

--- shell-master/base/var_replace.sh
+++ shell-master/base/var_replace.sh
@@ -4,98 +4,37 @@
 name="var_replace"
 value=""
 
-# ${var+msg}
-echo "\${var+msg}"
+
 # 有值
-echo ${name+"word"}
-echo $name
+echo "\${var+msg}: ${name+'word'} <=> echo $name"
+echo "\${var-msg}: ${name-'word'} <=> echo $name"
+echo "\${var=msg}: ${name='word'} <=> echo $name"
+echo "\${var?msg}: ${name?'word'} <=> echo $name"
+
+echo "\${var:+msg}: ${name:+'word'} <=> echo $name"
+echo "\${var:-msg}: ${name:-'word'} <=> echo $name"
+echo "\${var:=msg}: ${name:='word'} <=> echo $name"
+echo "\${var:?msg}: ${name:?'word'} <=> echo $name"
+
 # 无值
-echo ${value+"word"}
-echo $value
-# 无变量
-echo ${rep+"word"}
-echo $rep
-echo .
+echo "\${var+msg}: ${value+'word'} <=> echo $value"
+echo "\${var-msg}: ${value-'word'} <=> echo $value"
+echo "\${var=msg}: ${value='word'} <=> echo $value"
+echo "\${var?msg}: ${value?'word'} <=> echo $value"
 
-:<<!
-# ${var-msg}
-echo "\${var-msg}"
-# 有值
-echo ${name-"word"}
-echo $name
-# 无值
-echo ${value-"word"}
-echo $value
-# 无变量
-echo ${rep-"word"}
-echo $rep
-echo .
+echo "\${var:+msg}: ${value:+'word'} <=> echo $value"
+echo "\${var:-msg}: ${value:-'word'} <=> echo $value"
+echo "\${var:=msg}: ${value:='word'} <=> echo $value"
+echo "\${var:?msg}: ${value:?'word'} <=> echo $value"
 
-# ${var=msg}
-echo "\${var=msg}"
-echo ${name="word"}
-echo $name
-echo $value
-echo ${value="word"}
-echo ${rep="word"}
-echo $rep
-echo .
+# 无定义
+echo "\${var+msg}: ${rep+'word'} <=> echo $rep"
+echo "\${var-msg}: ${rep-'word'} <=> echo $rep"
+echo "\${var=msg}: ${rep='word'} <=> echo $rep"
+echo "\${var?msg}: ${rep?'word'} <=> echo $rep"
 
-# ${var?msg}
-name="var_replace"
-value=""
-echo "\${var?msg}"
-echo ${name?"var exist"}
-echo $name
-echo ${value?"var exist"}
-echo $value
-echo ${rep?"var not exist"}
-echo $rep
-echo .
-#!
-
-# ${var:+msg}
-echo "\${var:+msg}"
-echo ${name:+"word"}
-echo $name
-echo ${value:+"word"}
-echo $value
-echo ${rep:+"word"}
-echo $rep
-echo .
-
-#:<<!
-# ${var:-msg}
-echo "\${var:-msg}"
-echo ${name:-"word"}
-echo $name
-echo ${value:-"word"}
-echo $value
-echo ${rep:-"word"}
-echo $rep
-echo .
-
-# ${var:=msg}
-echo "\${var:=msg}"
-echo ${name:="word"}
-echo $name
-echo $value
-echo ${value:="word"}
-echo ${rep:="word"}
-echo $rep
-echo .
-
-# ${var:?msg}
-name="var_replace"
-value=""
-echo "\${var:?msg}"
-echo ${name:?"var exist"}
-echo $name
-echo ${value:?"var exist"}
-echo $value
-echo ${repl:?"var not exist"}
-echo $repl
-echo .
-!
-
+echo "\${var:+msg}: ${rep:+'word'} <=> echo $rep"
+echo "\${var:-msg}: ${rep:-'word'} <=> echo $rep"
+echo "\${var:=msg}: ${rep:='word'} <=> echo $rep"
+echo "\${var:?msg}: ${rep:?'word'} <=> echo $rep"
 
