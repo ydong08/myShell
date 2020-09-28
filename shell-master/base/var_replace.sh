ae813747df563ad3ad779ef2100cb0928b519212
ydong08 <lfeng1108@163.com>
2020-09-28 08:33:31 +0800

    update code

--- shell-master/base/var_replace.sh
+++ shell-master/base/var_replace.sh
@@ -6,37 +6,41 @@ value=""
 
 
 # 有值
-echo "\${var+msg}: ${name+'word'} <=> echo $name"
 echo "\${var-msg}: ${name-'word'} <=> echo $name"
 echo "\${var=msg}: ${name='word'} <=> echo $name"
 echo "\${var?msg}: ${name?'word'} <=> echo $name"
 
-echo "\${var:+msg}: ${name:+'word'} <=> echo $name"
 echo "\${var:-msg}: ${name:-'word'} <=> echo $name"
 echo "\${var:=msg}: ${name:='word'} <=> echo $name"
 echo "\${var:?msg}: ${name:?'word'} <=> echo $name"
-echo "-------------------------------------------\n"
+echo "-------------------------------------------"
 
 # 无值
-echo "\${var+msg}: ${value+'word'} <=> echo $value"
 echo "\${var-msg}: ${value-'word'} <=> echo $value"
 echo "\${var=msg}: ${value='word'} <=> echo $value"
 echo "\${var?msg}: ${value?'word'} <=> echo $value"
 
-echo "\${var:+msg}: ${value:+'word'} <=> echo $value"
 echo "\${var:-msg}: ${value:-'word'} <=> echo $value"
 echo "\${var:=msg}: ${value:='word'} <=> echo $value"
 echo "\${var:?msg}: ${value:?'word'} <=> echo $value"
-echo "-------------------------------------------\n"
+echo "-------------------------------------------"
 
 # 无定义
-echo "\${var+msg}: ${rep+'word'} <=> echo $rep"
 echo "\${var-msg}: ${rep-'word'} <=> echo $rep"
 echo "\${var=msg}: ${rep='word'} <=> echo $rep"
 echo "\${var?msg}: ${rep?'word'} <=> echo $rep"
 
-echo "\${var:+msg}: ${rep:+'word'} <=> echo $rep"
 echo "\${var:-msg}: ${rep:-'word'} <=> echo $rep"
 echo "\${var:=msg}: ${rep:='word'} <=> echo $rep"
 echo "\${var:?msg}: ${rep:?'word'} <=> echo $rep"
 
+
+# 例外
+echo "\${var+msg}: ${name+'word'} <=> echo $name"
+echo "\${var:+msg}: ${name:+'word'} <=> echo $name"
+
+echo "\${var+msg}: ${value+'word'} <=> echo $value"
+echo "\${var:+msg}: ${value:+'word'} <=> echo $value"
+
+echo "\${var+msg}: ${rep+'word'} <=> echo $rep"
+echo "\${var:+msg}: ${rep:+'word'} <=> echo $rep"
